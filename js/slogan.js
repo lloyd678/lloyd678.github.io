1827aee034c4ada93ceef7b23aa1211a24a31df7
Lloyd <lloydwilson94@gmail.com>
2020-05-14 11:18:54 +0100

    Add files via upload
    add slogan js

---
+++ js/slogan.js
@@ -0,0 +1,103 @@
+$(document).ready(function(){
+
+  var fontSize = [];
+  var mobileFontSize = [];
+
+  if($('#topBorder').height() == 60){
+    fontSize[0] ='150px';
+    fontSize[1] ='130px';
+    fontSize[2] ='80px';
+  }
+  else{
+    fontSize[0] ='100px';
+    fontSize[1] ='80px';
+    fontSize[2] ='55px';
+  }
+
+  $(".generateButton").click(function(){
+
+    message1 = $("#toplinemessage").val()
+    $("#topline").html(message1);
+    if(message1.length>24){
+      $('#topline').css("font-size", fontSize[2]);
+      $('#topline').css("line-height", fontSize[2]);
+      $('#topline').css("letter-spacing", "-2px");
+
+    }
+    else if(message1.length>13){
+      $('#topline').css("font-size", fontSize[1]);
+      $('#topline').css("line-height", fontSize[1]);
+      $('#topline').css("letter-spacing", "-3px");
+    }
+    else{
+      $('#topline').css("font-size", fontSize[0]);
+      $('#topline').css("line-height", fontSize[0]);
+      $('#topline').css("letter-spacing", "-4px");
+    }
+
+    message2 = $("#middlelinemessage").val()
+    $("#middleline").html(message2);
+    if(message2.length>24){
+      $('#middleline').css("font-size", fontSize[2]);
+      $('#middleline').css("line-height", fontSize[2]);
+      $('#middleline').css("letter-spacing", "-2px");
+    }
+    else if (message2.length>13){
+      $('#middleline').css("font-size", fontSize[1]);
+      $('#middleline').css("line-height", fontSize[1]);
+      $('#middleline').css("letter-spacing", "-3px");
+    }
+    else{
+      $('#middleline').css("font-size", fontSize[0]);
+      $('#middleline').css("line-height", fontSize[0]);
+      $('#middleline').css("letter-spacing", "-4px");
+    }
+
+    message3 = $("#bottomlinemessage").val()
+    $("#bottomline").html(message3);
+    if(message3.length>24){
+      $('#bottomline').css("font-size", fontSize[2]);
+      $('#bottomline').css("line-height", fontSize[2]);
+      $('#bottomline').css("letter-spacing", "-2px");
+    }
+    else if(message3.length>13){
+      $('#bottomline').css("font-size", fontSize[1]);
+      $('#bottomline').css("line-height", fontSize[1]);
+      $('#bottomline').css("letter-spacing", "-3px");
+    }
+    else{
+      $('#bottomline').css("font-size", fontSize[0]);
+      $('#bottomline').css("line-height", fontSize[0]);
+      $('#bottomline').css("letter-spacing", "-4px");
+    }
+
+  });
+
+  $(".saveButton").click(function(){
+    $("#imgOut").html("");
+    html2canvas(document.querySelector("#poster")).then(canvas => {
+      document.getElementById('imgOut').appendChild(canvas)
+      $(canvas).attr('id', 'savedCanvas');
+      var filename = "coronaslogan" + parseInt(Math.random()*1000000);
+      var theCanvas = document.getElementById('savedCanvas');
+      saveAs(theCanvas.toDataURL(), filename);
+    });
+  });
+
+
+
+});
+
+
+function saveAs(uri, filename) {
+    var link = document.createElement('a');
+    if (typeof link.download === 'string') {
+        link.href = uri;
+        link.download = filename;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    } else {
+        window.open(uri);
+    }
+}
